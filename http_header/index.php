dc5487f94017a5e402f431d3a18d6ba449211ed4
Rym <ryma.chaari@gmail.com>
2016-03-31 09:35:15 +0100

    Display index header and footer

---
+++ http_header/index.php
@@ -0,0 +1,11 @@
+<?php 
+$wikipediaURL = 'http://localhost/drupal-7.43/?q=http_header';
+/* $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $wikipediaURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Le blog de Samy Dindane (www.dinduks.com)');
+$resultat = curl_exec ($ch);
+curl_close($ch);*/
+$resultat = file_get_contents($wikipediaURL);
+echo $resultat;
+?>
