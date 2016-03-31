7121b66e27a55e41362f8b8952df7e64c8adb228
Rym <ryma.chaari@gmail.com>
2016-03-31 09:45:36 +0100

    URL HEADER AND FOOTER

--- http_header/index.php
+++ http_header/index.php
@@ -1,11 +1,11 @@
 <?php 
-$wikipediaURL = 'http://localhost/drupal-7.43/?q=http_header';
+$AUF_URL_header = 'http://auf.eg-vm-preprod2.accelance.net/?q=node/1';
 /* $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $wikipediaURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Le blog de Samy Dindane (www.dinduks.com)');
 $resultat = curl_exec ($ch);
 curl_close($ch);*/
-$resultat = file_get_contents($wikipediaURL);
+$resultat = file_get_contents($AUF_URL_header);
 echo $resultat;
 ?>
