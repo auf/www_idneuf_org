7aa0311d44289473680ce5588b5df632fb99e6c7
eb2-rchaari <rchaari@evolutive-group.com>
2016-04-01 16:38:12 +0100

    index header

--- http_header/index.php
+++ http_header/index.php
@@ -1,6 +1,6 @@
 
 <?php
-// cette fonction técupère le contenu d'un document distant
+// cette fonction tÃ©cupÃ¨re le contenu d'un document distant
 function geturl($laurl){
         $data=null;
         if($http= fopen($laurl,"r")){
@@ -13,7 +13,7 @@ function geturl($laurl){
 }
 
 
-// cette fonction récupère le contenu des tags sélectionnés
+// cette fonction rÃ©cupÃ¨re le contenu des tags sÃ©lectionnÃ©s
 function get_tag1($file,$tag="header"){
    preg_match_all("/(<".$tag.".*>)(\w.*)(<\/".$tag.">)/ismU",$file,$patterns);
        return $patterns;
@@ -22,10 +22,10 @@ function get_tag1($file,$tag="header"){
 ?>
 <?php
 // recuperer le contenu de cette page
-$data=geturl("http://localhost/www_idneuf_org/?q=http_header");
-// récupère le contenu du tag, tag compris
+$data=geturl("http://".$_SERVER['SERVER_NAME']."/?q=http_header");
+// rÃ©cupÃ¨re le contenu du tag, tag compris
 $extrait=get_tag1($data,"header");
-// ce qui revient est un tableau, on liste tous les résultats avec foreach
+// ce qui revient est un tableau, on liste tous les rÃ©sultats avec foreach
 foreach($extrait[0] as $contenu){
 echo $contenu;        
 }
