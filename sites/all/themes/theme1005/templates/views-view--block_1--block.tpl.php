2d71a81d08000d0ab775f012e40fc0e35a8236c6
eb2-rchaari <rchaari@evolutive-business.com>
2016-05-05 16:44:04 +0100

    responsive design 1

--- sites/all/themes/theme1005/templates/views-view--block_1--block.tpl.php
+++ sites/all/themes/theme1005/templates/views-view--block_1--block.tpl.php
@@ -1,10 +1,10 @@
-<div id="ori-oai-search" >
-<?php
-$content_search=file_get_contents('http://testv2.ori-oai.org/idneuf-dev/index.html?bloc=simple-search&includeJs=false&includeJsLight=true&addMainDiv=false');
-echo $content_search;?>
-<br>
-<?php
-$content_nbr=file_get_contents('http://testv2.ori-oai.org/idneuf-dev/index.html?bloc=title-with-nb-results&ajax=false&includeAjaxParameters=false&addMainDiv=false');
-echo $content_nbr;
-?>
+<div id="ori-oai-search" style="font-size:18px !important;" >
+    <?php
+    $content_search=file_get_contents('http://testv2.ori-oai.org/idneuf-dev/index.html?bloc=simple-search&includeJs=false&includeJsLight=true&addMainDiv=false');
+    echo $content_search;?>
+    <br>
+    <?php
+    $content_nbr=file_get_contents('http://testv2.ori-oai.org/idneuf-dev/index.html?bloc=title-with-nb-results&ajax=false&includeAjaxParameters=false&addMainDiv=false');
+    echo $content_nbr;
+    ?>
 </div>
